d6459e075e78b6c2cc66b94b1f6cbdc05b76b510
Brian Ruff <briananthonyruff@gmail.com>
2021-07-19 19:37:15 -0400

    update port env

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,6 @@ const app = express();
 
 middleware(app);
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(process.env.PORT || 3000, () =>
+   console.log('Listening on port ' + process.env.PORT)
+);
